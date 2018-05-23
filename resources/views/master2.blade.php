cbe1badfd460d154003a4777f306f9d63bcf9325
Unknown <apriliakhairunnisa98@gmail.com>
2018-05-23 22:51:17 +0700

    tambah error

--- resources/views/master2.blade.php
+++ resources/views/master2.blade.php
@@ -5,9 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--===============================================================================================-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+  <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{asset('assets/images/icons/favicon.png')}}"/>
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/themify/themify-icons.css')}}">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/lightbox2/css/lightbox.min.css">
+  <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/util.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/main.css')}}">
   </head>
-  <body>
-    <div class="container-fluid">
+  <body class=" animsition">
+    <div class="container">
       <div class="header clearfix">
         @yield ('header')
       </div>
@@ -27,5 +56,30 @@
     </div>
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="/assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/bootstrap/js/popper.js"></script>
+  <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/daterangepicker/moment.min.js"></script>
+  <script type="text/javascript" src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/slick/slick.min.js"></script>
+  <script type="text/javascript" src="{{asset('assets/js/slick-custom.js')}}"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/parallax100/parallax100.js"></script>
+  <script type="text/javascript">
+        $('.parallax100').parallax100();
+  </script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+  <script type="text/javascript" src="vendor/lightbox2/js/lightbox.min.js"></script>
+<!--===============================================================================================-->
+  <script src="js/main.js"></script>
   </body>
 </html>
